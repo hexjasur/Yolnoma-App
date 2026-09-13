6235ef5c8cb50d4d4934050dab46545ae200a626
hexjasur <hexjasur@gmail.com>
2026-09-13 23:03:44 +0500

    optimazed images

--- src/shared/assets/images.ts
+++ src/shared/assets/images.ts
@@ -7,7 +7,7 @@ import logo_icon from '../../assets/brand/icon.ico';
 // VI Page
 import VI_HERO_IMG from '../../assets/gta6/hero_image_gta6.avif';
 import VI_LOGO_IMG from '../../assets/gta6/gta6logo.webp';
-import VI_LEONIDA from '../../assets/gta6/leonida.png';
+import VI_LEONIDA from '../../assets/gta6/leonida.jpg';
 import VI_THOP from '../../assets/gta6/twop.jpg';
 import VI_VC_LEONIDA from '../../assets/gta6/vcleonida.jpg';
 import VI_JasonLucia from '../../assets/gta6/j&l.jpg';
